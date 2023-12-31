9b1c28e0b79f29e5da7dd1271e053fa6726fcde3
tharanga270 <developerwordpress270@gmail.com>
2023-12-31 16:53:41 +0530

    1.client Navbaar complete

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Navbaar from './components/header/Navbaar';
+
+function App() {
+  return (
+    <>
+      <Navbaar />
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/components/newnavbaar/Newnav.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Newnav = () => {
+  return <div></div>;
+};
+
+export default Newnav;
