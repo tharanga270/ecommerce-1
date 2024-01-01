847007648797e8fbe56fd24f3a6731bb6a698a97
tharanga270 <developerwordpress270@gmail.com>
2024-01-01 14:26:56 +0530

    2.client ul and main slider complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Navbaar from './components/header/Navbaar';
+import Newnav from './components/newnavbaar/Newnav';
+import Maincomp from './components/home/Maincomp';
 
 function App() {
   return (
     <>
       <Navbaar />
+      <Newnav />
+      <Maincomp />
     </>
   );
 }
